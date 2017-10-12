59ca823400b976594258b1e4cd28cdd77039e513
rajat-vig <rajatvig71@gmail.com>
2017-10-12 01:11:31 -0400

    Install Bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show, :delete]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 	
 	
 	def index
